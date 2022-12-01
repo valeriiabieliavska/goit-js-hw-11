a56ed0eba26139cecb28823f40801792b521f417
valeriiabieliavska <106553939+valeriiabieliavska@users.noreply.github.com>
2022-12-01 16:23:37 +0200

    markup gallery, fetchimages

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
 import './styles.css';
-import axios, { Axios } from 'axios';
+import axios from 'axios';
 import Notiflix from 'notiflix';
 import SimpleLightbox from 'simplelightbox';
 import "simplelightbox/dist/simple-lightbox.min.css";
 
-const KEY = '31704253-3506fb69b26df966a85a65283';
-const BASE_URL = 'https://pixabay.com/api/';
-
 const form = document.querySelector('.search-form');
 const gallery = document.querySelector('.gallery');
+
+
+
+// function renderImg(images) {
+//     const markup = images.map(image => {
+//         return `<div class="photo-card">
+//         <a href="${image.largeImageURL}">
+//   <img class="image" src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
+//   </a>
+//   <div class="info">
+//     <p class="info-item"><span>${image.likes}</span>
+//       <b>Likes</b>
+//     </p>
+//     <p class="info-item"><span>${image.views}</span>
+//       <b>Views</b>
+//     </p>
+//     <p class="info-item"><span>${image.comments}</span>
+//       <b>Comments</b>
+//     </p>
+//     <p class="info-item"><span>${image.downloads}</span>
+//       <b>Downloads</b>
+//     </p>
+//   </div>
+// </div>`
+//     }).join('');
+//     gallery.insertAdjacentHTML('beforeend', markup);
+//     console.log(markup);
+// }
+
 
